e1148e31d30500353c7e13b9750f41905e29148a
Aigars Cibulskis <azael05x@gmail.com>
2021-02-22 15:05:42 +0200

    Implemented tutorial

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rust guessing game
+Going through and implementing [Rust book tutorial](https://doc.rust-lang.org/stable/book/ch02-00-guessing-game-tutorial.html#setting-up-a-new-project)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let number = rand::thread_rng().gen_range(1..101);
+
+    println!("Guess the number!");
+    println!("Please input your guess.");
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number, please write numeric");
+                continue;
+            },
+        };
+
+        match guess.cmp(&number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
